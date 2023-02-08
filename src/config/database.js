23f4646241c4b1402b44de2e9ad7c975fe4385de
Bruno Morais <brunomoraisads@gmail.com>
2023-02-08 19:57:27 -0300

    configuração inicial do projeto

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "56157469",
+    database: "crud",
+    define: {
+        timestamp: true,
+        underscored: true
+    }
+}
