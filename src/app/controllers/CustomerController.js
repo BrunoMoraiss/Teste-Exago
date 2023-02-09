1fbd5a557447df1822b6188d281d50473a2ddab2
Bruno Morais <brunomoraisads@gmail.com>
2023-02-09 11:40:57 -0300

    Criação controller Customers

---
+++ src/app/controllers/CustomerController.js
@@ -0,0 +1,19 @@
+class CustomerController{
+    async index (req, res){
+
+    }
+
+    async craete (req, res){
+        
+    }
+
+    async update (req, res){
+        
+    }
+
+    async destroy (req, res){
+        
+    }
+}
+
+module.exports = new CustomerController()
